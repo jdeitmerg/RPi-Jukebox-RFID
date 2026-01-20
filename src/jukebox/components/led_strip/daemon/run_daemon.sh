79a67b0ad21876d52ab930a0aad563fe9ca79e8c
Jonas Deitmerg <jonasdeitmerg@aim.com>
2026-01-20 10:13:10 +0100

    Add requirements to base installation

--- src/jukebox/components/led_strip/daemon/run_daemon.sh
+++ src/jukebox/components/led_strip/daemon/run_daemon.sh
@@ -2,11 +2,12 @@
 
 cd "$(dirname "$0")"
 
-LOGFILE=../../../../../shared/logs/led_strip_daemon.log
+INSTALL_DIR=../../../../..
+LOGFILE=$INSTALL_DIR/shared/logs/led_strip_daemon.log
 
 # Make sure log will be readable by non-root users
 touch $LOGFILE
 chmod 664 $LOGFILE
 
-. .venv/bin/activate
+. $INSTALL_DIR/.venv/bin/activate
 python3 led_strip_daemon.py $@ 2>&1 | tee $LOGFILE
